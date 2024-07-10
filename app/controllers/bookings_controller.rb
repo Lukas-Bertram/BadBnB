1b7d3f6333713b5c7cdda058efb7189d3e4cf618
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-10 15:23:58 +0200

    bookings index modified

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,31 @@
 class BookingsController < ApplicationController
-  def index
+  before_action :set_offer, only: %i[new create index]
 
+  def index
+    @bookings = Booking.all
   end
 
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    @booking.save
+    redirect_to offer_path(@offer)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
   end
 
   def booking_params
-    require.params(:booking).permit(:check_in, :ckeck_out)
+    params.require(:booking).permit(:offer_id, :check_in, :check_out)
   end
 end
