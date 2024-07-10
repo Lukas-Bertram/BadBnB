4560f60743b5f4fcc6decbb56cccb02395ef719c
Rahel Pauli <rahel.pauli@gmx.de>
2024-07-10 13:11:17 +0200

    started implementing new and create actions booking, also added label to forms

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_offer, only: %i[new create]
 
   def new
-
+    @booking = Booking.new
   end
-  
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.offer = @offer
+    @booking.save
+    redirect_to offer_path(@offer)
+  end
+
   def show
 
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out)
   end
 end
